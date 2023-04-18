5e7096594cceb919448bc29a8ba7b88fe06bf7ef
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-04-18 02:38:16 -0300

    :wrench: add eslint settings

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,7 +1,7 @@
-export * as colors from "./colors";
-export * as fontSizes from "./font-sizes";
-export * as fontWeights from "./font-weights";
-export * as fonts from "./fonts";
-export * as lineHeights from "./line-heights";
-export * as radii from "./radii";
-export * as space from "./space";
+export * as colors from './colors'
+export * as fontSizes from './font-sizes'
+export * as fontWeights from './font-weights'
+export * as fonts from './fonts'
+export * as lineHeights from './line-heights'
+export * as radii from './radii'
+export * as space from './space'
