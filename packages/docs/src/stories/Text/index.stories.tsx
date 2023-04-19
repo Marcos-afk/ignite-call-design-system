ba9937a01e2c76e1ec02af8ed60250d281ea89b5
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-04-19 13:57:50 -0300

    :memo: add documentation of Text component

---
+++ packages/docs/src/stories/Text/index.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem ipsam incidunt porro! Exercitationem cumque deleniti blanditiis quam soluta molestiae placeat, in recusandae laudantium accusamus impedit itaque, fugit vitae dolores dolorum?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
