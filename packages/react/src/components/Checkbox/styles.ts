04d5c5c3a494af2476047f164125d1e02112e1a5
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-04-19 23:22:46 -0300

    :memo: add documentation of Checkbox component

---
+++ packages/react/src/components/Checkbox/styles.ts
@@ -0,0 +1,49 @@
+import * as CheckBox from '@radix-ui/react-checkbox'
+import { styled, keyframes } from '../../styles'
+
+export const Container = styled(CheckBox.Root, {
+  all: 'unset',
+  width: '$6',
+  height: '$6',
+  backgroundColor: '$gray900',
+  border: '2px solid $gray900',
+  borderRadius: '$xs',
+  lineHeight: 0,
+  cursor: 'pointer',
+  boxSizing: 'border-box',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  '&[data-state="checked"]': {
+    backgroundColor: '$ignite300',
+  },
+
+  '&:focus': {
+    borderColor: '$ignite300',
+  },
+})
+
+const slideIn = keyframes({
+  from: { transform: 'translateY(-100%)' },
+  to: { transform: 'translateY(0)' },
+})
+
+const slideOut = keyframes({
+  from: { transform: 'translateY(0)' },
+  to: { transform: 'translateY(100%)' },
+})
+
+export const Indicator = styled(CheckBox.Indicator, {
+  color: '$white',
+  width: '$4',
+  height: '$4',
+
+  '&[data-state="checked"]': {
+    animation: `${slideIn} 200ms ease-out`,
+  },
+
+  '&[data-state="unchecked"]': {
+    animation: `${slideOut} 200ms ease-out`,
+  },
+})
