ab88a3c5ce50df0d0d81c8ade6dbea4eebc0d709
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-04-19 22:42:27 -0300

    :memo: add documentation of TextInput component

---
+++ packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps } from 'react'
+import * as S from './styles'
+
+export interface TextInputProps extends ComponentProps<typeof S.Input> {
+  prefix?: string
+}
+
+export const TextInput = ({ prefix, ...props }: TextInputProps) => {
+  return (
+    <S.Container>
+      {!!prefix && <S.Prefix>{prefix}</S.Prefix>}
+      <S.Input {...props} />
+    </S.Container>
+  )
+}
